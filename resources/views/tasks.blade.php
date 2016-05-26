5ad51ea657c0f7226dbe5e7b0695ec3e2f8c13bf
Abby-Liu <wellliu0302@gmail.com>
2016-05-26 23:23:57 +0800

    Add and Show Tasks

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -32,5 +32,37 @@
         </form>
     </div>
 
-    <!-- 待辦：目前任務 -->
+    <!-- undone：目前任務 -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                目前任務
+            </div>
+        </div>
+
+        <div class="panel-body">
+            <table class="table table-striped task-table">
+                    <!-- 表頭 -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp</th>
+                    </thead>
+
+                    <!-- 表身 -->
+                    <tbody>
+                        @foreach ($task as $task)
+                        <tr>
+                            <!-- 任務名稱 -->
+                            <td class="table-text">
+                                <div>
+                                    {{ $task->name }}
+                                </div>
+                            </td>
+
+                            <!-- 待辦：刪除按鈕-->
+                    </tbody>
+            </table>
+
+        </div>
+        @endif
 @endsection
